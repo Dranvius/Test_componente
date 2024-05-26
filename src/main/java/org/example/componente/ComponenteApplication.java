42dd1547e042ce1987565f73096bfa364afc98b3
sergio linares <dranvius12@hotmail.com>
2024-05-26 10:37:03 -0500

    primera_entrega_26_may_2024

---
+++ src/main/java/org/example/componente/ComponenteApplication.java
@@ -0,0 +1,13 @@
+package org.example.componente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/componente/ComponenteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.componente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
